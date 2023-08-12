9d05ea98e8aa50ae9241a3031ea071904bad70a6
deepak <d.bhatia@friendlyscore.com>
2023-08-12 19:55:54 +0100

    create campaign

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -18,4 +18,21 @@ contract CrowdFunding {
 
     uint256 public numberOfCampaigns = 0;
 
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(campaign.deadline < block.timestamp, "The end of the crowdfunding campaign should be a date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+    }
 }
