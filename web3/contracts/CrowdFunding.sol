4808ed9a3608bdace838b30e651660415f4af391
deepak <d.bhatia@friendlyscore.com>
2023-08-12 20:37:30 +0100

    get campaigns

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -35,4 +35,16 @@ contract CrowdFunding {
 
         return numberOfCampaigns - 1;
     }
+
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for(uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
+    }
 }
