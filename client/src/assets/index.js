f7377f9d084b3eccfc4a9ec92df5ee5f95b07da1
deepak <d.bhatia@friendlyscore.com>
2023-08-13 08:42:02 +0100

    add icons, navbar changes

---
+++ client/src/assets/index.js
@@ -0,0 +1,31 @@
+import createCampaign from './create-campaign.png';
+import dashboard from './dashboard.png';
+import crowdfunding from './crowdfunding.png';
+import logout from './logout.png';
+import payment from './payment.png';
+import profile from './profile.png';
+import success from './success.png';
+import withdraw from './withdraw.png';
+import tagType from './type.png';
+import search from './search.png';
+import menu from './menu.png';
+import money from './money.png';
+import loader from './loader.png';
+import thirdweb from './thirdweb.png';
+
+export {
+  tagType,
+  createCampaign,
+  dashboard,
+  crowdfunding,
+  logout,
+  payment,
+  profile,
+  success,
+  withdraw,
+  search,
+  menu,
+  money,
+  loader,
+  thirdweb,
+};
