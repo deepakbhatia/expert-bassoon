0c9d70be1384e4423b80440325d58dbb7e550fcc
deepak <d.bhatia@friendlyscore.com>
2023-08-13 11:42:26 +0100

    background colors

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import { useStateContext } from '../context';
 import { CustomButton } from './';
-import { crowdfunding, menu, search, thirdweb } from '../assets';
+import {crowdfunding, menu, profile, search} from '../assets';
 import { navlinks } from '../constants';
 
 const Navbar = () => {
@@ -14,7 +14,7 @@ const Navbar = () => {
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
-      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
+      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#DDE6ED] rounded-[100px]">
         <input type="text" placeholder="Search for campaigns" className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none" />
         
         <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
@@ -34,15 +34,15 @@ const Navbar = () => {
         />
 
         <Link to="/profile">
-          <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
-            <img src={thirdweb} alt="user" className="w-[60%] h-[60%] object-contain" />
+          <div className="w-[52px] h-[52px] rounded-full flex justify-center items-center cursor-pointer">
+            <img src={profile} alt="user" className="w-[60%] h-[60%] object-contain" />
           </div>
         </Link>
       </div>
 
       {/* Small screen navigation */}
         <div className="sm:hidden flex justify-between items-center relative">
-        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+        <div className="w-[40px] h-[40px] rounded-[10px]  flex justify-center items-center cursor-pointer">
             <img src={crowdfunding} alt="user" className="w-[60%] h-[60%] object-contain" />
           </div>
 
@@ -53,7 +53,7 @@ const Navbar = () => {
             onClick={() => setToggleDrawer((prev) => !prev)}
           />
 
-          <div className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${!toggleDrawer ? '-translate-y-[100vh]' : 'translate-y-0'} transition-all duration-700`}>
+          <div className={`absolute top-[60px] right-0 left-0  z-10 shadow-secondary py-4 ${!toggleDrawer ? '-translate-y-[100vh]' : 'translate-y-0'} transition-all duration-700`}>
             <ul className="mb-4">
               {navlinks.map((link) => (
                 <li
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import {crowdfunding, success} from '../assets';
 import { navlinks } from '../constants';
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
-  <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
+  <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#DDE6ED]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
     {!isActive ? (
       <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
     ) : (
@@ -21,10 +21,10 @@ const Sidebar = () => {
   return (
     <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
       <Link to="/">
-        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={crowdfunding} />
+        <Icon styles="w-[52px] h-[52px]" imgUrl={crowdfunding} />
       </Link>
 
-      <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
+      <div className="flex-1 flex flex-col justify-between items-center bg-[#81a32] rounded-[20px] w-[76px] py-4 mt-12">
         <div className="flex flex-col justify-center items-center gap-3">
           {navlinks.map((link) => (
             <Icon 
@@ -41,7 +41,7 @@ const Sidebar = () => {
           ))}
         </div>
 
-        <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={success} />
+        <Icon styles="shadow-secondary" imgUrl={success} />
       </div>
     </div>
   )
